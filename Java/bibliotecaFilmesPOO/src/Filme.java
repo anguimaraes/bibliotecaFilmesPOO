0fb2f65cbff7da21e8bbf0d590ac789de3aa5fc7
anguimaraes <andregtst@gmail.com>
2024-10-17 08:01:00 -0300

    criando metodos de exibir ficha tecnica, avaliação e resgate de média

--- Java/bibliotecaFilmesPOO/src/Filme.java
+++ Java/bibliotecaFilmesPOO/src/Filme.java
@@ -2,7 +2,21 @@ public class Filme {
     String nome;
     int anoDeLancamento;
     boolean incluidoNoPlano;
-    double avaliacao;
+    double somaDasAvaliacoes;
     int totalDeAvaliacoes;
     int duracaoEmMinutos;
+
+    void exibeFichaTecnica(){
+        System.out.println("O nome do filme é: " + nome);
+        System.out.println("O ano de lançamento é: " + anoDeLancamento);
+    }
+
+    void avalia(double nota){
+        somaDasAvaliacoes += nota;
+        totalDeAvaliacoes ++;
+    }
+
+    double pegaMedia(){
+        return somaDasAvaliacoes / totalDeAvaliacoes;
+    }
 }
